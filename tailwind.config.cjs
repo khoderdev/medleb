dfecb422f17fedb8c9883b0425e7b5e118085e24
Khoder <132526568+khoderdev@users.noreply.github.com>
2023-12-30 05:08:15 +0200

    Settingup the Desktop mini and Normal Sidebar-1

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,83 +1,10 @@
-// // module.exports = {
-// //   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
-// //   darkMode: "class",
-
-// //   theme: {
-// //     screens: {
-// //       xs: "366px",
-// //       // => @media (min-width: 366px)
-// //       sm: "640px",
-// //       // => @media (min-width: 640px)
-
-// //       md: "768px",
-// //       // => @media (min-width: 768px)
-
-// //       lg: "1024px",
-// //       // => @media (min-width: 1024px)
-
-// //       xl: "1280px",
-// //       // => @media (min-width: 1280px)
-
-// //       "2xl": "1536px",
-// //       // => @media (min-width: 1536px)
-// //     },
-
-// //     extend: {},
-// //   },
-// //   variants: {},
-// //   plugins: [
-// //     // require("daisyui"),
-// //     require("tailwindcss-animate"),
-// //     require("@tailwindcss/forms")({
-// //       strategy: "base",
-// //     }),
-// //   ],
-// // };
-
-// module.exports = {
-//   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
-//   darkMode: "class",
-//   theme: {
-//     screens: {
-//       xs: "366px",
-//       sm: "640px",
-//       md: "768px",
-//       lg: "1024px",
-//       xl: "1280px",
-//       "2xl": "1536px",
-//     },
-//     extend: {
-//       colors: {
-//         // Add your custom color palette here
-//         siteBackground: {
-
-//           500: "#FBF9FA",
-
-//         contentsBackground: {
-
-//           500: "#F5F5F5"
-
-//         },
-//       },
-//     },
-//   },
-//   variants: {},
-//   plugins: [
-//     require("tailwindcss-animate"),
-//     require("@tailwindcss/forms")({
-//       strategy: "base",
-//     }),
-//   ],
-// };
-// }
-
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   darkMode: "class",
   theme: {
     screens: {
       xs: "366px",
-      sm: "640px",
+      // sm: "640px",
       md: "768px",
       lg: "1024px",
       xl: "1280px",
